a7b848d2e1b098cec596f5b9ea69670793cbf259
Vinícius A. Santos <e.viniciusas@gmail.com>
2022-04-14 11:26:00 -0300

    Preparado arquivo de teste com envio e recebimento de arquivos

---
+++ test-cloudrun/test-file.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<http.ByteStream> processImage(Uint8List bytes) async {
+  http.Request request = http.Request(
+    'POST',
+    Uri.parse('http://localhost:8080'),
+    // Uri.parse('https://recognizer-usnztkx52q-rj.a.run.app')
+  );
+  request.headers.addAll({
+    'Keep-Alive': 'timeout=100, max=100',
+  });
+  request.bodyBytes = bytes;
+  http.StreamedResponse response = await request.send();
+  return response.stream;
+}
+
+Future<File> testSingle() async {
+  File img = File('test.jpg');
+  File res = File('result.jpg');
+  IOSink writeStream = res.openWrite();
+  await writeStream.addStream(await processImage(await img.readAsBytes()));
+  await writeStream.close();
+  return res;
+}
+
+void testSeveral(List<String> args) async {
+  File img = File(args[0]);
+  var bytes = await img.readAsBytes();
+  var tests = [];
+  for (int i = 0; i < 1; i++) {
+    print("starting $i");
+    tests.add(processImage(bytes));
+    await Future.delayed(Duration(milliseconds: 500));
+  }
+  for (var test in tests){
+    print("awaiting");
+    var response = await test;
+    print("response: $response");
+  }
+}
+
+void main(List<String> args){
+  testSingle();
+  //testSeveral(args);
+}
